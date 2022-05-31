67a9932b664e6dba16f62e959ab6a0b64f9534fd
abbbn <bery.baban@ms.bulme.at>
2022-05-31 08:05:40 +0200

    test

--- Forms/OrderDeleteForm.py
+++ Forms/OrderDeleteForm.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.datetime import DateField
-from wtforms.fields.simple import BooleanField, StringField, TextAreaField
-from wtforms.fields import DecimalField, SelectField
+from wtforms.fields.simple import StringField
+from wtforms.fields
 from wtforms import validators
 
 class OrderDeleteForm(FlaskForm):
