074c44a21d3f0d2a8d20d78caf10b4b6199df3a2
abbbn <bery.baban@ms.bulme.at>
2022-05-31 08:52:09 +0200

    import added

--- controllers/products.py
+++ controllers/products.py
@@ -3,6 +3,6 @@ import sqlalchemy
 
 from forms.ProductDeleteForm import ProductDeleteForm
 from forms.ProductForm import ProductForm
-from model.models import Product, Productline, db, Customer
+from model.models import Auto
 
 products_blurprint = Blueprint('products_blueprint', __name__)
